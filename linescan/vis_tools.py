4881fe60de071bb307fff5f4f97e6c0b3ed501c2
Langer,Christoph <christoph.langer@imba.oeaw.ac.at>
2022-02-21 11:26:03 +0100

    Add 4 channels support

--- linescan/vis_tools.py
+++ linescan/vis_tools.py
@@ -16,8 +16,8 @@ def plot_image_and_profiles(
     """
     if number_of_channels == 2:
         plot_image_and_profiles_2c(image_path, roi_path, disp_channels=disp_channels)
-    elif number_of_channels == 3:
-        plot_image_and_profiles_3c(image_path, roi_path, disp_channels=disp_channels)
+    elif number_of_channels >= 3:
+        plot_image_and_profiles_3c(image_path, roi_path, disp_channels=disp_channels, number_of_channels=number_of_channels)
     else:
         print(
             f"The channel number {number_of_channels}, is not supported in this version"
@@ -87,14 +87,17 @@ def plot_image_and_profiles_3c(
     for key, item in roi.items():
         slice_set.add(item["position"]["slice"])
     slice_set = sorted(slice_set)
-
+    print(number_of_channels)
+    print(disp_channels)
+    print(slice_set)
     fig, axs = plt.subplots(
+        #TODO:Fix this 
         len(slice_set),
         number_of_channels + len(disp_channels),
         figsize=(15, 5 * len(slice_set)),
     )
     image = io.imread(image_path)
-    for round, img_slice in enumerate(slice_set):
+    for slice_num, img_slice in enumerate(slice_set):
         # Plot linescans of all channels
         for channel in range(number_of_channels):
             for key, item in roi.items():
@@ -109,26 +112,36 @@ def plot_image_and_profiles_3c(
                         mode="constant"
                         # TODO merge with 2c since this is the only difference
                     )
-                    axs[round, channel].plot(range(len(y)), y)
+                    if len(slice_set) > 1:
+                        axs[slice_num, channel].plot(range(len(y)), y)
+                    else: axs[channel].plot(range(len(y)), y)
         # Draw images with lines for the set of channels to display
         for disp_num, disp_channel in enumerate(disp_channels):
-            axs[round, number_of_channels + disp_num].imshow(
-                image[img_slice, :, :, disp_channel], cmap="gray"
-            )
+            if len(slice_set) > 1:
+                axs[slice_num, number_of_channels + disp_num].imshow(
+                    image[img_slice, :, :, disp_channel], cmap="gray"
+                )
+            else:
+                axs[number_of_channels + disp_num].imshow(
+                    image[img_slice, :, :, disp_channel], cmap="gray"
+                )
             for key, item in roi.items():
                 if item["position"]["slice"] == img_slice:
                     x_values = [item["x1"], item["x2"]]
                     y_values = [item["y1"], item["y2"]]
-                    axs[round, number_of_channels + disp_num].plot(x_values, y_values)
+                    if len(slice_set) > 1:
+                        axs[slice_num, number_of_channels + disp_num].plot(x_values, y_values)
+                    else: axs[number_of_channels + disp_num].plot(x_values, y_values)
 
 
+# TODO: Merge Image would be nice
 # TODO: Also do not draw lineprofile if not displayed
 def plot_line_profiles(image_path, roi_path, disp_channels=None, number_of_channels=3):
     """Plots every every linescan together with the image slice it was drawn on.
     Colors represent the individual channels, the line drawn is diplayed on top of the image.
     The line profiles can be normalized, and the fitting and peak calling can be displayed.
     Values are in pixel unless the user provides the pixel size in microns."""
-    if number_of_channels == 2 or number_of_channels == 3:
+    if number_of_channels == 2 or number_of_channels == 3 or number_of_channels == 4:
         plot_line_profiles_2c(
             image_path,
             roi_path,
@@ -177,7 +190,8 @@ def plot_line_profiles_2c(
                 values = measure.profile_line(
                     image[img_slice, channel, :, :], src, dst, 1, mode="constant"
                 )
-            if number_of_channels == 3:
+            #TODO: This is not clean
+            elif number_of_channels >= 3:
                 values = measure.profile_line(
                     image[img_slice, :, :, channel], src, dst, 1, mode="constant"
                 )
@@ -195,7 +209,8 @@ def plot_line_profiles_2c(
                 axs[item_num, 1 + disp_num].imshow(
                     image[img_slice, disp_channel, :, :], cmap="gray"
                 )
-            if number_of_channels == 3:
+            #TODO: This is not clean
+            elif number_of_channels >= 3:
                 axs[item_num, 1 + disp_num].imshow(
                     image[img_slice, :, :, disp_channel], cmap="gray"
                 )
@@ -215,7 +230,8 @@ def plot_line_profiles_2c(
                 axs[item_num, 1 + disp_num].imshow(
                     image[img_slice, disp_channel, :, :], cmap="gray"
                 )
-            if number_of_channels == 3:
+            #TODO: This is not clean
+            elif number_of_channels >= 3:
                 axs[item_num, 1 + disp_num].imshow(
                     image[img_slice, :, :, disp_channel], cmap="gray"
                 )
